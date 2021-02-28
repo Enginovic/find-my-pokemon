c48c7fadcd61d1ba1c4a359092f458e3558560f7
Engin Sasmaz <engins.93@gmail.com>
2021-02-28 19:34:12 +0100

    added vuetify, axios & lodash

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+	<div>
+		<input @input="debounceMe" />
+		<v-alert type="success"></v-alert>
+		<div v-for="pokemon in pokemonList" :key="pokemon.id">
+			{{ pokemon.name }}
+		</div>
+	</div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
-</script>
+<script src="./Home.ts" lang="ts"></script>
